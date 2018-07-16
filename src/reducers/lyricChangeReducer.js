e3526ade73dda8d03c7875dbf7f3551971387c5a
Jimmy McNamara <jmcnamara44@gmail.com>
2018-07-16 15:57:40 -0700

    build karaoke using middleware and advanced redux in week 4.

--- src/reducers/lyricChangeReducer.js
+++ src/reducers/lyricChangeReducer.js
@@ -22,6 +22,21 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
       [action.currentSongId]: newSongsByIdEntry
     });
     return newSongsByIdStateSlice;
+  case types.REQUEST_SONG:
+    newSongsByIdEntry = Object.assign({}, state[action.songId], {
+      isFetching: true,
+      receivedAt: action.receivedAt,
+      title: action.title,
+      artist: action.artist,
+      songArray: action.songArray,
+      arrayPosition: 0,
+      songId: action.songId
+    });
+    newSongsByIdStateSlice = Object.assign({}, state, {
+      [action.songId]: newSongsByIdEntry
+    });
+    return newSongsByIdStateSlice;
+
   default:
     return state;
   }
